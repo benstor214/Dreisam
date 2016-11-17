3110fefc7bd39f6742693586f52b482b73e53863
Lumpenstein <jhang_90@hotmail.com>
2016-11-17 12:45:16 +0100

    Jhang eischen upload

---
+++ DreisamApplication/app/src/main/java/com/example/benoit/dreisamapplication/MenuActivity.java
@@ -0,0 +1,43 @@
+package com.example.benoit.dreisamapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    Button btn_phone, btn_map, btn_logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        //init Views
+        init();
+
+
+        btn_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+    private void init() {
+        btn_phone= (Button) findViewById(R.id.btn_menu_phone);
+        btn_map= (Button) findViewById(R.id.btn_menu_map);
+        btn_logout= (Button) findViewById(R.id.btn_menu_logout);
+    }
+}
