e1b65f58d566e7abeda1dafa6348d475cf187280
benstor214 <benoit.storck@gmail.com>
2016-11-17 14:34:06 +0100

    Bereis dë et schons mat mir zë schaffen?

--- DreisamApplication/app/src/main/java/com/example/benoit/dreisamapplication/LoginActivity.java
+++ DreisamApplication/app/src/main/java/com/example/benoit/dreisamapplication/LoginActivity.java
@@ -2,12 +2,30 @@ package com.example.benoit.dreisamapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEditText, passwordEditText;
+    private RelativeLayout mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mainView = (RelativeLayout) findViewById(R.id.activity_login);
+
+        loginEditText = (EditText) findViewById(R.id.etxt_login);
+        passwordEditText = (EditText) findViewById(R.id.etxt_pswd);
+
+        findViewById(R.id.btn_signin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
